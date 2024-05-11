1ff103d3129711f25c96630a99d03993dc05d8d4
nabihahhh <35028821+nabihahhh@users.noreply.github.com>
2024-05-11 22:36:25 +0800

    added pagination 10 max per page

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NgxPaginationModule],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
@@ -14,6 +15,7 @@ export class DashboardComponent {
 
   employeesData: any;
   filteredEmployeeData: any[] = [];
+  p: number = 1;
 
   ngOnInit() {
     const baseURL = "https://api.sampleapis.com/futurama/characters";
@@ -21,7 +23,6 @@ export class DashboardComponent {
     .then(resp => resp.json())
     .then(data => 
       this.employeesData = data
-
     );
     this.filteredEmployeeData = this.employeesData;
     console.log(this.employeesData)
