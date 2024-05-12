58006d257283b8b0ba6b20342d62de1e7d620192
nabihahhh <35028821+nabihahhh@users.noreply.github.com>
2024-05-12 20:09:29 +0800

    added search by name

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,9 @@ import { Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { NgArrayPipesModule } from 'ngx-pipes';
+import { BehaviorSubject, Observable, combineLatest, map } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,10 +16,17 @@ import { NgxPaginationModule } from 'ngx-pagination';
 
 export class DashboardComponent {
 
-  employeesData: any;
+  employeesData: any[]= [];
   filteredEmployeeData: any[] = [];
+  originalData: any[] = [];
   isFiltered: boolean = false;
   p: number = 1;
+  searchValue: string = "";
+  speciesData: string[] = ["Human", "Martian", "Robot", "Mutant", "Decapodian", "Omicronian", "Amphibiosans"];
+  genderData: string[] = ["Male", "Female"];
+  // searchTerm = "";
+  // users$!: Observable<any[]>;
+  // filteredUsers$!: Observable<any[]>;
 
   ngOnInit() {
     const baseURL = "https://api.sampleapis.com/futurama/characters";
@@ -25,19 +35,28 @@ export class DashboardComponent {
     .then(data => 
       this.employeesData = data
     ).then(data =>
-      this.filteredEmployeeData = this.employeesData
+      this.employeesData.forEach(e=> {
+        this.filteredEmployeeData.push({id: e.id, name: e.name.first + " " + e.name.middle + " " + e.name.last, species: e.species, gender: e.gender, image: e.images.main, occupation: e.occupation});
+      })
+    ).then (data => 
+      this.originalData = this.filteredEmployeeData
     );
 
   }
 
-  
-  filterSearch(data: any[], filterProperty: string, filter: any){
-    this.filteredEmployeeData = [];
+  assignSearchValue (filter: any){
+    this.searchValue = (filter.target as HTMLTextAreaElement).value.toLowerCase();
+  }
+
+  filterSearch(data: any[]){
     this.isFiltered = true;
-    const filterValue = filter.toString().toLowerCase();
-    this.filteredEmployeeData = filterValue != 'backspace' ? data.filter(item => item.name.first.includes(filterValue)) || 
-    data.filter(item => item.name.middle.includes(filterValue)) || data.filter(item => item.name.last.includes(filterValue))
-    : data;
+    this.filteredEmployeeData = this.searchValue != 'backspace'? data.filter(item => item.name.toLowerCase().includes(this.searchValue))
+    : this.originalData;
+  }
+
+  clearFilter() {
+    this.isFiltered = false;
+    this.filteredEmployeeData = this.originalData;
   }
 
 }
